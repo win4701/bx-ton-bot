7748f0a1c0cbd9aad4e760590d11ba943bba8b1e
Mr Adam <cfellag1@gmail.com>
2025-12-30 22:49:27 +0100

    Delete src/src directory

--- src/src/antifraud.js
+++
@@ -1,7 +0,0 @@
-import { todayWithdrawn } from "./db.js";
-
-export async function canWithdraw(tid, amount) {
-  if (amount <= 0) return false;
-  const used = await todayWithdrawn(tid);
-  return used + amount <= Number(process.env.DAILY_WITHDRAW_LIMIT);
-}
