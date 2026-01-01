a828b66be99483a475ff1d6d3d662aa1096e8577
Mr Adam <cfellag1@gmail.com>
2026-01-01 11:51:19 +0100

    Create offers.js

---
+++ src/economy/offers.js
@@ -0,0 +1,17 @@
+// src/economy/offers.js
+export const OFFERS = {
+  TON: [
+    { pay:10, receive:20 },
+    { pay:20, receive:45 },
+    { pay:50, receive:120 },
+    { pay:100, receive:260 },
+    { pay:250, receive:700 }
+  ],
+  USDT: [
+    { pay:10, receive:15 },
+    { pay:25, receive:40 },
+    { pay:50, receive:90 },
+    { pay:100, receive:200 },
+    { pay:250, receive:600 }
+  ]
+};
