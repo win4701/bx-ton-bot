102ef7fc37bbf48f724af85a4a2688f0df2225b6
Mr Adam <cfellag1@gmail.com>
2026-01-02 13:16:56 +0100

    Update price.js

--- src/ton/price.js
+++ src/ton/price.js
@@ -1,7 +1,11 @@
-export async function getBxTonPrice() {
-  // call ston.fi API
+import fetch from "node-fetch";
+
+export async function getBxPrice() {
+  const r = await fetch("https://api.ston.fi/v1/pools/BX-TON");
+  const j = await r.json();
   return {
-    price: 0.2,
-    change24h: "+4.1%"
+    price: j.price,
+    change24h: j.change24h,
+    liquidity: j.liquidity
   };
 }
